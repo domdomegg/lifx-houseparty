f1d6fe4d34a3f80b341243d906ee397113236ec1
Adam Jones <jones_adam@rocketmail.com>
2020-11-25 14:09:54 +0000

    Extract types to types.ts

---
+++ server/src/types.ts
@@ -0,0 +1,43 @@
+import type { App } from './app';
+
+export { App };
+
+export interface Bindings {
+    transformers: { [key: string]: Transformer },
+    sources: { [key: string]: Source }
+}
+
+export interface Setupable {
+    setup: (app: App) => void;
+}
+
+export interface Teardownable {
+    teardown: (app: App) => void;
+}
+
+export interface HSBKColor {
+    hue: number;
+    saturation: number;
+    brightness: number;
+    kelvin?: number;
+}
+
+export type Waveform = 'SAW' | 'SINE' | 'HALF_SINE' | 'TRIANGLE' | 'PULSE';
+
+export interface Light {
+    setColor(params: { color: HSBKColor, duration?: number, callback?: () => void }): void;
+    setWaveform(params: { fromColor?: HSBKColor, toColor: HSBKColor, transient?: boolean, period: number, cycles?: number, skewRatio?: number, waveform: Waveform, callback?: () => void }): void;
+}
+
+export interface Source extends Setupable, Teardownable {
+    key: string;
+    onData?: (app: App, data: any) => void;
+}
+
+export interface Transformer extends Setupable, Teardownable {
+    key: string;
+    handleBeat?: (app: App) => void;
+    onLightConnect?: (app: App, light: Light) => void;
+    onLightDisconnect?: (app: App, light: Light) => void;
+    onData?: (app: App, data: any) => void;
+}

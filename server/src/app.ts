cd770c2404a120acfea35e8ee4ce1ef0551d7e64
Adam Jones <jones_adam@rocketmail.com>
2020-11-25 14:09:54 +0000

    Extract types to types.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Nothing as NothingTransformer } from './transformer/sourceless/nothing';
 import { Nothing as NothingSource } from './source/nothing';
+import { Light, Source, Transformer, Bindings, Setupable, Teardownable } from './types';
 
 export class App {
     lights: Light[] = [];
@@ -105,43 +106,4 @@ export class App {
 
         if (this.transformer.handleBeat) this.transformer.handleBeat(this)
     }
-}
-
-interface Bindings {
-    transformers: { [key: string]: Transformer },
-    sources: { [key: string]: Source }
-}
-
-interface Setupable {
-    setup: (app: App) => void;
-}
-interface Teardownable {
-    teardown: (app: App) => void;
-}
-
-export interface HSBKColor {
-    hue: number;
-    saturation: number;
-    brightness: number;
-    kelvin?: number;
-}
-
-export type Waveform = 'SAW' | 'SINE' | 'HALF_SINE' | 'TRIANGLE' | 'PULSE';
-
-export interface Light {
-    setColor(params: { color: HSBKColor, duration?: number, callback?: () => void }): void;
-    setWaveform(params: { fromColor?: HSBKColor, toColor: HSBKColor, transient?: boolean, period: number, cycles?: number, skewRatio?: number, waveform: Waveform, callback?: () => void }): void;
-}
-
-export interface Source extends Setupable, Teardownable {
-    key: string;
-    onData?: (app: App, data: any) => void;
-}
-
-export interface Transformer extends Setupable, Teardownable {
-    key: string;
-    handleBeat?: (app: App) => void;
-    onLightConnect?: (app: App, light: Light) => void;
-    onLightDisconnect?: (app: App, light: Light) => void;
-    onData?: (app: App, data: any) => void;
 }

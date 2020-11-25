e513fe5d0051976a7eadf6fbef1e039be331c486
Adam Jones <jones_adam@rocketmail.com>
2020-11-25 13:18:46 +0000

    Add rainbowBeats.colorPerBulb option

--- server/src/transformer/rainbowBeats.ts
+++ server/src/transformer/rainbowBeats.ts
@@ -4,11 +4,16 @@ export class RainbowBeats implements Transformer {
     static key: string = 'rainbowBeats';
     key: string = RainbowBeats.key;
 
+    private options = { colorPerBulb: false };
+
     private t: number;
     
     handleBeat(app: App) {
-        app.lights.forEach(light => light.setColor({ color: { hue: (this.t * 90) % 360, saturation: 100, brightness: 100 }}));
-        this.t++;
+        app.lights.forEach(light => {
+            light.setColor({ color: { hue: (this.t * 90) % 360, saturation: 100, brightness: 100 }})
+            if (this.options.colorPerBulb) this.t++;
+        });
+        if (!this.options.colorPerBulb) this.t++;
     }
 
     setup(app: App) {
